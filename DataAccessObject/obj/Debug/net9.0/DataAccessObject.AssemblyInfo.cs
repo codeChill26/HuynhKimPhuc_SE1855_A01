49b83e6ceeb56335f16d19a2aac0fee9fb10f3d8
PhucCodeChill <kphuchuynh2005@gmail.com>
2025-06-27 00:13:12 +0700

    Đổi tên

--- DataAccessObject/obj/Debug/net9.0/DataAccessObject.AssemblyInfo.cs
+++ DataAccessObject/obj/Debug/net9.0/DataAccessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d348a29af9adecbbd900431fdd293765675a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
